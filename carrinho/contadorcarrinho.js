00641da78368eb095351879d7c86a81d2351b20a
Guilherme Henrique <guihenriquesilva10@gmail.com>
2023-11-22 21:55:06 -0300

    modifiqueiocontador

--- carrinho/contadorcarrinho.js
+++ carrinho/contadorcarrinho.js
@@ -1,20 +1,19 @@
-let documentadiocionar = document.getElementById("contadorquant");
-let quantidadeSpan = document.getElementById("quantidade");
-let maisButton = document.getElementById("mais");
-let menosButton = document.getElementById("menos")
+    let documentadiocionar = document.getElementById("contadorquant");
+    let quantidadeSpan = document.getElementById("quantidade");
+    let maisButton = document.getElementById("mais");
+    let menosButton = document.getElementById("menos")
 
-function addmais1(event) {
-    let quantidadeAtual = parseInt(quantidadeSpan.textContent);
-    quantidadeSpan.textContent = quantidadeAtual + 1;
-}
+    function addmais1(event) {
+      let quantidadeAtual = parseInt(quantidadeSpan.textContent);
+      quantidadeSpan.textContent = quantidadeAtual + 1;
+    }
 
-function addmenos1(params) {
-   if (quantidadeSpan.textContent > 0) {
-    let quantidadeAtual = parseInt(quantidadeSpan.textContent);
-    quantidadeSpan.textContent = quantidadeAtual - 1;
-   }
-}
+    function addmenos1(params) {
+      if (quantidadeSpan.textContent > 0) {
+        let quantidadeAtual = parseInt(quantidadeSpan.textContent);
+        quantidadeSpan.textContent = quantidadeAtual - 1;
+      }
+    }
 
-
-maisButton.addEventListener("click", addmais1);
-menosButton.addEventListener("click", addmenos1);
+    maisButton.addEventListener("click", addmais1);
+    menosButton.addEventListener("click", addmenos1);
